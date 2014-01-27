92d9e9885e241e0bb7df64d3cd696db09cdfc74d
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-27 11:20:12 -0200

    Remove columns filter on pandas_to_dict

--- utils.py
+++ utils.py
@@ -9,6 +9,5 @@ def fix_str(value):
 
 def pandas_to_dict(df):
     return [{colname: (fix_str(row[i]) if type(row[i]) is str else row[i])
-             for i, colname in enumerate(df.columns)
-             if colname not in ['pedido_data', 'cliente_data']}
+             for i, colname in enumerate(df.columns)}
             for row in df.values]
