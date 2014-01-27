5eec7466c704e39dc282855e7a4af90af142313e
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-27 18:41:29 -0200

    Create forms for admin

---
+++ admin/forms.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import riak
+
+from wtforms.fields import TextField, TextAreaField, SelectField
+from wtforms.validators import Required
+from wtforms_tornado import Form
+
+
+class ConnectionForm(Form):
+    name = TextField(validators=[Required()])
+    conection = TextField(validators=[Required()])
+
+
+class CubeForm(Form):
+    myClient = riak.RiakClient(protocol='http',
+                               http_port=8098,
+                               host='127.0.0.1')
+    myBucket = myClient.bucket('openmining-admin')
+
+    bconnection = myBucket.get('connection').data
+
+    CONNECTION = tuple([(c['slug'], c['name']) for c in bconnection])
+
+    name = TextField(validators=[Required()])
+    conection = SelectField(choices=CONNECTION, validators=[Required()])
+    sql = TextAreaField(validators=[Required()])
