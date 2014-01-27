d5bb959e5afcb2e94ca04e9a36d7eb6ef5f038b7
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-27 11:18:19 -0200

    accepted API URL dynamic filter

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,16 @@
-angular.module('OpenMining', [])
-.value('API_URL', '/process.json?fields=')
+angular.module('OpenMining', ['ngRoute'])
+.value('API_URL', '/process.json?')
 
 .controller('Process',
-  function($scope, $http, API_URL) {
+  function($scope, $http, $location, API_URL) {
+    for (var key in $location.search()){
+      API_URL += key + "=" + $location.search()[key] + "&";
+    }
+    console.log(API_URL)
     $scope.loading = true;
     $http({method: 'POST', url: API_URL}).
       success(function(data, status, headers, config) {
+        console.log(data)
         $scope.process = data.json;
         $scope.columns = data.columns;
         $scope.predicate = "-id_cliente";
