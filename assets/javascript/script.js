b78efffc59734c35d32203cbee468f96f6b296d6
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-27 14:26:56 -0200

    Remove console log

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,11 +6,9 @@ angular.module('OpenMining', [])
     for (var key in $location.search()){
       API_URL += key + "=" + $location.search()[key] + "&";
     }
-    console.log(API_URL)
     $scope.loading = true;
     $http({method: 'POST', url: API_URL}).
       success(function(data, status, headers, config) {
-        console.log(data)
         $scope.process = data.json;
         $scope.columns = data.columns;
         $scope.loading = false;
