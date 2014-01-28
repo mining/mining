bbbf4f9e185aa09abdbcc710417df7acd6249f56
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-28 14:00:06 -0200

    Use dynamic process (json) ajax load

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,18 @@
 angular.module('OpenMining', [])
-.value('API_URL', '/process.json?')
-
 .controller('Process',
-  function($scope, $http, $location, API_URL) {
-    for (var key in $location.search()){
-      API_URL += key + "=" + $location.search()[key] + "&";
-    }
+  function($scope, $http, $location) {
     $scope.loading = true;
-    $http({method: 'POST', url: API_URL}).
-      success(function(data, status, headers, config) {
-        $scope.process = data.json;
-        $scope.columns = data.columns;
-        $scope.loading = false;
-      });
+    $scope.init = function(slug) {
+      API_URL = "/process/" + slug + ".json?";
+      for (var key in $location.search()){
+        API_URL += key + "=" + $location.search()[key] + "&";
+      };
+
+      $http({method: 'POST', url: API_URL}).
+        success(function(data, status, headers, config) {
+          $scope.process = data.json;
+          $scope.columns = data.columns;
+          $scope.loading = false;
+        });
+    };
 });
