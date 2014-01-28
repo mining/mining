2b84115e45aa9dcd9e46741f96a6a4aaa80cbe9c
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-28 14:53:06 -0200

    Add log on bin load data

--- bin.py
+++ bin.py
@@ -33,70 +33,31 @@ for cube in radmin.get('cube').data:
     e = create_engine(connection)
     connection = e.connect()
 
-    print sql
     resoverall = connection.execute(sql)
 
+    print "# LOAD DATA ON DATAWAREHOUSE: {}".format(slug)
     df = DataFrame(resoverall.fetchall())
     df.columns = resoverall.keys()
     df.head()
 
     convert = pandas_to_dict(df)
 
+    print "# SAVE DATA (JSON) ON RIAK: {}".format(slug)
     join = json.dumps(convert)
     b1 = rmining.new(slug, data=join)
     b1.store()
 
-    b2 = rmining.new(u'{}-columns'.format(slug), data=json.dumps([c for c in df.columns]))
+    print "# SAVE COLUMNS ON RIAK: {}".format(slug)
+    b2 = rmining.new(u'{}-columns'.format(slug),
+                     data=json.dumps([c for c in df.columns]))
     b2.store()
 
+    print "# SAVE CONNECT ON RIAK: {}".format(slug)
     b3 = rmining.new(u'{}-connect'.format(slug), data=c)
     b3.store()
 
+    print "# SAVE SQL ON RIAK: {}".format(slug)
     b4 = rmining.new(u'{}-sql'.format(slug), data=sql)
     b4.store()
 
-exit(0)
-
-
-mc = memcache.Client(['127.0.0.1:11211'], debug=0)
-mc.delete('testando')
-mc.delete('testando-columns')
-
-myClient = riak.RiakClient(protocol='http', http_port=8098, host='127.0.0.1')
-myBucket = myClient.bucket('openmining')
-
-c = 'mysql://root:123mudar@192.168.12.4/upessencia_dev1'
-e = create_engine(c)
-connection = e.connect()
-
-sql = """SELECT
-cliente.id_cliente,
-cliente.nome,
-pedido.id_pedido,
-pedido.criacao_ts as 'pedido_data',
-cliente.criacao_ts as 'cliente_data'
-FROM pedido
-inner join cliente on cliente.id_cliente = pedido.id_cliente
-WHERE pedido.id_pedido_status NOT IN (4,8,9)
-AND pedido.entrega_forma<>'franquia' limit 100"""
-
-resoverall = connection.execute(sql)
-
-df = DataFrame(resoverall.fetchall())
-df.columns = resoverall.keys()
-df.head()
-
-convert = pandas_to_dict(df)
-
-join = json.dumps(convert)
-b1 = myBucket.new('testando', data=join)
-b1.store()
-
-b2 = myBucket.new('testando-columns', data=json.dumps([c for c in df.columns]))
-b2.store()
-
-b3 = myBucket.new('testando-connect', data=c)
-b3.store()
-
-b4 = myBucket.new('testando-sql', data=sql)
-b4.store()
+print "## FINISH"
