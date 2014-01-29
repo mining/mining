5a970ee7e192bdd2378236b7a2a5de8487459979
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-29 18:09:33 -0200

    Mock chart in highchart

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-angular.module('OpenMining', [])
+angular.module('OpenMining', ["highcharts-ng"])
 .controller('Process',
   function($scope, $http, $location) {
     $scope.loading = true;
@@ -14,5 +14,57 @@ angular.module('OpenMining', [])
           $scope.columns = data.columns;
           $scope.loading = false;
         });
+    };
+})
+
+
+.controller('Chart',
+  function($scope, $http, $location) {
+    $scope.loading = true;
+    $scope.init = function(slug) {
+      API_URL = "/process/" + slug + ".json?";
+      for (var key in $location.search()){
+        API_URL += key + "=" + $location.search()[key] + "&";
+      };
+
+      $http({method: 'POST', url: API_URL}).
+        success(function(data, status, headers, config) {
+          $scope.process = data.json;
+          $scope.columns = data.columns;
+          $scope.loading = false;
+        });
+
+
+
+        $scope.chartConfig = {
+          options: {
+            chart: {
+              type: 'line',
+              zoomType: 'x'
+            }
+          },
+          series: [
+            {
+              name: "Testando meta dados 1",
+              data: [10002, 15987, 12987, 82222, 77622, 16236, 17833, 19787, 15223, 10123]
+            },
+            {   
+              name: "Testando meta dados 2",
+              data: [11762, 212319, 231231, 52760, 212330, 10000, 123220]
+            }
+          ],
+          title: {
+            text: 'Hello'
+          },
+          credits: {
+            enabled: false
+          },
+          xAxis: {currentMin: 0, currentMax: 10, minRange: 1},
+          loading: false
+        }
+
+
+
+
     };
 });
