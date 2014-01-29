9b311cae78a52650ca5f3913e8a6d7af18a3aae6
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-29 15:06:19 -0200

    Create new admin view, ElementHandler create/update element via admin

--- admin/views.py
+++ admin/views.py
@@ -7,12 +7,63 @@ import tornado.web
 import tornado.gen
 
 from mining.utils import slugfy
-from admin.forms import ConnectionForm, CubeForm
+from admin.forms import ConnectionForm, CubeForm, ElementForm
 
 
 class AdminHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('admin/base.html')
+
+
+class ElementHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self, slug=None):
+        form = ElementForm()
+        myClient = riak.RiakClient(protocol='http',
+                                   http_port=8098,
+                                   host='127.0.0.1')
+        myBucket = myClient.bucket('openmining-admin')
+
+        get_bucket = myBucket.get('element').data
+        if get_bucket is None:
+            get_bucket = []
+
+        for bload in get_bucket:
+            if bload['slug'] == slug:
+                for k in form._fields:
+                    getattr(form, k).data = bload[k]
+
+        self.render('admin/element.html',
+                    form=form,
+                    element=get_bucket,
+                    slug=slug)
+
+    def post(self, slug=None):
+        form = ElementForm(self.request.arguments)
+        if not form.validate():
+            self.set_status(400)
+            self.write(form.errors)
+
+        myClient = riak.RiakClient(protocol='http',
+                                   http_port=8098,
+                                   host='127.0.0.1')
+        myBucket = myClient.bucket('openmining-admin')
+
+        data = form.data
+        data['slug'] = slugfy(data.get('name'))
+
+        get_bucket = [b for b in myBucket.get('element').data or []
+                      if b['slug'] != data['slug']]
+        if get_bucket is None:
+            get_bucket = []
+        get_bucket.append(data)
+
+        b1 = myBucket.new('element', data=get_bucket)
+        for k in data:
+            b1.add_index("{}_bin".format(k), data[k])
+        b1.store()
+
+        self.redirect('/admin/element')
 
 
 class CubeHandler(tornado.web.RequestHandler):
@@ -30,9 +81,8 @@ class CubeHandler(tornado.web.RequestHandler):
 
         for bload in get_bucket:
             if bload['slug'] == slug:
-                form.sql.data = bload['sql']
-                form.conection.data = bload['conection']
-                form.name.data = bload['name']
+                for k in form._fields:
+                    getattr(form, k).data = bload[k]
 
         self.render('admin/cube.html', form=form, cube=get_bucket, slug=slug)
 
