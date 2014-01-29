74d944c10efe09ad2e08ef1047ba6aa80188231d
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-29 17:44:03 -0200

    fix get cache (memcached) process result

--- mining/views.py
+++ mining/views.py
@@ -62,7 +62,7 @@ class ProcessHandler(tornado.web.RequestHandler):
         fields_json = json.dumps(fields)
         if mc.get(str(slug)) and\
                 mc.get('{}-columns'.format(slug)) == fields_json:
-            self.write(mc.get(slug))
+            self.write(mc.get(str(slug)))
             self.finish()
 
         mc.set('{}-columns'.format(slug), fields_json)
