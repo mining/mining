5dd624c027cedb0588f92dd899b69c2cd536d3d4
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-29 15:04:33 -0200

    Create new forms, Element and Dashboard

--- admin/forms.py
+++ admin/forms.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 import riak
 
-from wtforms.fields import TextField, TextAreaField, SelectField
+from wtforms.fields import TextField, TextAreaField, HiddenField
+from wtforms.fields import SelectField, SelectMultipleField
 from wtforms.validators import Required
 from wtforms_tornado import Form
 
@@ -34,3 +35,20 @@ class CubeForm(Form):
                             validators=[Required()])
     sql = TextAreaField(validators=[Required()])
 
+
+class ElementForm(Form):
+    ELEMENT_TYPE = ((u'grid', u'Grid'),
+                    (u'chart_line', u'Chart line'),
+                    (u"chart_bar", u"Chart bar"))
+
+    name = TextField(validators=[Required()])
+    type = SelectField(choices=ELEMENT_TYPE, validators=[Required()])
+    cube = SelectField(choices=ObjGenerate('cube', 'slug', 'name'),
+                       validators=[Required()])
+
+
+class DashboardForm(Form):
+    name = TextField(validators=[Required()])
+    element = SelectMultipleField(
+        choices=ObjGenerate('element', 'slug', 'name'),
+        validators=[Required()])
