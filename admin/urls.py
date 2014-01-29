9b0d547b3c726bca818a4992d064e8abb4fd7017
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-29 15:45:52 -0200

    Create dashboard admin

--- admin/urls.py
+++ admin/urls.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from .views import AdminHandler, CubeHandler, ConnectionHandler
-from .views import ElementHandler
+from .views import ElementHandler, DashboardHandler
 
 
 INCLUDE_URLS = [
@@ -9,4 +9,5 @@ INCLUDE_URLS = [
     (r"/admin/connection", ConnectionHandler),
     (r"/admin/cube/?(?P<slug>[\w-]+)?", CubeHandler),
     (r"/admin/element/?(?P<slug>[\w-]+)?", ElementHandler),
+    (r"/admin/dashboard/?(?P<slug>[\w-]+)?", DashboardHandler),
 ]
--- admin/views.py
+++ admin/views.py
@@ -7,12 +7,65 @@ import tornado.web
 import tornado.gen
 
 from mining.utils import slugfy
-from admin.forms import ConnectionForm, CubeForm, ElementForm
+from admin.forms import ConnectionForm, CubeForm, ElementForm, DashboardForm
 
 
 class AdminHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('admin/base.html')
+
+
+class DashboardHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self, slug=None):
+        form = DashboardForm()
+        myClient = riak.RiakClient(protocol='http',
+                                   http_port=8098,
+                                   host='127.0.0.1')
+        myBucket = myClient.bucket('openmining-admin')
+
+        get_bucket = myBucket.get('dashboard').data
+        if get_bucket is None:
+            get_bucket = []
+
+        for bload in get_bucket:
+            if bload['slug'] == slug:
+                for k in form._fields:
+                    getattr(form, k).data = bload[k]
+
+        self.render('admin/dashboard.html',
+                    form=form,
+                    dashboard=get_bucket,
+                    slug=slug)
+
+    def post(self, slug=None):
+        form = DashboardForm(self.request.arguments)
+        if not form.validate():
+            self.set_status(400)
+            self.write(form.errors)
+
+        myClient = riak.RiakClient(protocol='http',
+                                   http_port=8098,
+                                   host='127.0.0.1')
+        myBucket = myClient.bucket('openmining-admin')
+
+        data = form.data
+        data['slug'] = slugfy(data.get('name'))
+
+        get_bucket = [b for b in myBucket.get('dashboard').data or []
+                      if b['slug'] != data['slug']]
+        if get_bucket is None:
+            get_bucket = []
+        get_bucket.append(data)
+
+        b1 = myBucket.new('dashboard', data=get_bucket)
+        """
+        for k in data:
+            b1.add_index("{}_bin".format(k), data[k])
+        """
+        b1.store()
+
+        self.redirect('/admin/dashboard')
 
 
 class ElementHandler(tornado.web.RequestHandler):
