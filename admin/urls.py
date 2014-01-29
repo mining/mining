e5c1cccaa08b519a19b6900db1376f2b75113668
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-29 15:07:16 -0200

    Add Element admin view in admin url

--- admin/urls.py
+++ admin/urls.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from .views import AdminHandler, CubeHandler, ConnectionHandler
+from .views import ElementHandler
+
 
 INCLUDE_URLS = [
     (r"/admin", AdminHandler),
     (r"/admin/connection", ConnectionHandler),
     (r"/admin/cube/?(?P<slug>[\w-]+)?", CubeHandler),
+    (r"/admin/element/?(?P<slug>[\w-]+)?", ElementHandler),
 ]
