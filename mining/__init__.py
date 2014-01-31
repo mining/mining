950b46fadbcd33d1245a94b0044abcf669747d7b
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-01-31 18:06:10 -0200

    Create setup file

---
+++ mining/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+VERSION = (0, 1, 0)
+
+__version__ = ".".join(map(str, VERSION))
+__status__ = "Alpha"
+__description__ = "BI Application Server written by Python and Riak"
+__author__ = "Thiago Avelino <thiago@avelino.xxx>"
+__email__ = "thiago@avelino.xxx"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2013, Open Mining"
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+import mining
+
+
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()
+                if not i.startswith("http")]
+
+dependency_links = [i.strip() for i in open("requirements.txt").readlines()
+                    if i.startswith("http")]
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Environment :: Web Environment",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: JavaScript",
+    "Programming Language :: Python :: 2.7",
+    'Programming Language :: Python',
+    "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+try:
+    long_description = open('README.md').read()
+except:
+    long_description = mining.__description__
+
+setup(name='mining',
+      version=mining.__version__,
+      description=mining.__description__,
+      long_description=long_description,
+      classifiers=classifiers,
+      keywords='open mining bi business intelligence platform riak memcache',
+      author=mining.__author__,
+      author_email=mining.__email__,
+      url='http://openmining.io',
+      download_url="https://github.com/avelino/mining/tarball/master",
+      license=mining.__license__,
+      packages=find_packages(exclude=('doc', 'docs',)),
+      namespace_packages=['mining'],
+      package_dir={'mining': 'mining'},
+      install_requires=REQUIREMENTS,
+      dependency_links=dependency_links,
+      scripts=['bin/mining'],
+      include_package_data=True)
