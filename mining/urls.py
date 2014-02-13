e32be307fd99c0d38b514385e0af2c257a50d50b
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-13 19:16:06 -0200

    Add URL enter in dynamic process

--- mining/urls.py
+++ mining/urls.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from .views import MainHandler, ProcessHandler, DashboardHandler
+from .views import ProcessWebSocketHandler
 
 
 INCLUDE_URLS = [
+    (r"/process/(?P<slug>[\w-]+).ws", ProcessWebSocketHandler),
+    (r"/process/(?P<slug>[\w-]+).json", ProcessHandler),
     (r"/process/(?P<slug>[\w-]+).json", ProcessHandler),
     (r"/(?P<slug>[\w-]+)", DashboardHandler),
     (r"/", MainHandler),
