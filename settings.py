754418d1d2f2fd0f7d10b223299bcaa8e2b5fe08
yupbank <yupbank@gmail.com>
2014-02-15 00:51:10 +0800

    add setting file

---
+++ settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#riak settings
+RIAK_PROTOCOL = 'http'
+RIAK_HTTP_PORT = 8098
+RIAK_HOST = '127.0.0.1'
+
+ADMIN_BUCKET_NAME = 'openminig-admin'
+MINING_BUCKET_NAME = 'openmining'
+
+
+#memcache settings
+MEMCACHE_CONNECTION = '127.0.0.1:11211'
+MEMCACHE_DEBUG = 0
