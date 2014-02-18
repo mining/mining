9ddb1ccbcdb7eae138907f709c55ee8336522a3b
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-18 19:22:09 -0300

    add support in datetime (type) on utils fix type

--- mining/utils.py
+++ mining/utils.py
@@ -3,7 +3,7 @@
 import unicodedata
 import re
 from decimal import Decimal
-from datetime import date
+from datetime import date, datetime
 
 from pandas import tslib, date_range
 
@@ -16,7 +16,7 @@ def fix_type(value):
             return unicode(value.decode('latin1'))
     elif type(value) is tslib.Timestamp:
         return value.strftime("%Y-%m-%d %H:%M:%S")
-    elif type(value) is date:
+    elif type(value) is date or type(value) is datetime:
         return value.strftime("%Y-%m-%d")
     elif type(value) is Decimal:
         return float(value)
