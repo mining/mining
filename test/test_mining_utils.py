2ff1c8f40a7d9cfe943e48bfff49b76d1e2f7473
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-18 19:03:52 -0300

    writting test fix type str in latin1

--- test/test_mining_utils.py
+++ test/test_mining_utils.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from mining.utils import slugfy
+from mining.utils import slugfy, fix_type
 
 
 class df_slugfy_test(unittest.TestCase):
@@ -12,3 +12,10 @@ class df_slugfy_test(unittest.TestCase):
     def test_used_accents(self):
         self.assertEquals(u"testando-se-e-slugfy",
                           slugfy(u"Testando sé é slugfy"))
+
+
+class fix_type_test(unittest.TestCase):
+    def test_str_latin1(self):
+        self.assertEquals(fix_type("test".encode('latin1')), u"test")
+        self.assertEqual(type(fix_type("test".encode("latin1"))), unicode)
+
