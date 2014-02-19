8ee313482bfa21778dd9a5775b3b918be316aa78
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-19 11:57:33 -0300

    writting test df generate moc test between

--- test/test_mining_utils.py
+++ test/test_mining_utils.py
@@ -4,9 +4,9 @@ import unittest
 
 from datetime import date, datetime
 from decimal import Decimal
-from pandas import tslib
+from pandas import tslib, DataFrame
 
-from mining.utils import slugfy, fix_type, fix_render
+from mining.utils import slugfy, fix_type, fix_render, df_generate
 
 
 class df_slugfy_test(unittest.TestCase):
@@ -53,3 +53,24 @@ class fix_render_test(unittest.TestCase):
         data = [{'h': 1, 'v': 'a'}, {'h': 1, 'v': 'a'}]
         for d in data:
             self.assertEquals(fix_render(d), {'h': 1, 'v': u'a'})
+
+
+class df_generate_test(unittest.TestCase):
+    def test_between_date(self):
+        df = DataFrame([{'date': '2014-01-01'},
+                        {'date': '2014-02-01'},
+                        {'date': '2014-03-01'}])
+        g = df_generate(df, "2014-01-01:2014-02-01",
+                        "filter__date__between__date__:Y-:m-:d")
+
+        self.assertEquals(g, u"date in ['2014-01-01', '2014-01-02', "
+                          "'2014-01-03', '2014-01-04', '2014-01-05', "
+                          "'2014-01-06', '2014-01-07', '2014-01-08', "
+                          "'2014-01-09', '2014-01-10', '2014-01-11', "
+                          "'2014-01-12', '2014-01-13', '2014-01-14', "
+                          "'2014-01-15', '2014-01-16', '2014-01-17', "
+                          "'2014-01-18', '2014-01-19', '2014-01-20', "
+                          "'2014-01-21', '2014-01-22', '2014-01-23', "
+                          "'2014-01-24', '2014-01-25', '2014-01-26', "
+                          "'2014-01-27', '2014-01-28', '2014-01-29', "
+                          "'2014-01-30', '2014-01-31', '2014-02-01']")
