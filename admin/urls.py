270af43ffbe8974698d17ff6d5cae20fbf410f73
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-20 17:00:04 -0300

    Add url enter delete element on riak

--- admin/urls.py
+++ admin/urls.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from .views import CubeHandler, ConnectionHandler
+from .views import CubeHandler, ConnectionHandler, DeleteHandler
 from .views import ElementHandler, DashboardHandler, APIElementCubeHandler
 
 
 INCLUDE_URLS = [
+    (r"/admin/delete/(?P<bucket>[\w-]+)/(?P<slug>[\w-]+)", DeleteHandler),
     (r"/admin/connection/?(?P<slug>[\w-]+)?", ConnectionHandler),
     (r"/admin/cube/?(?P<slug>[\w-]+)?", CubeHandler),
     (r"/admin/api/element/cube/?(?P<slug>[\w-]+)?", APIElementCubeHandler),
