0d98211023a044665f9ffb8989ab2cccfafe8f94
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-20 12:01:59 -0300

    Add api application on urls mining root

--- urls.py
+++ urls.py
@@ -6,9 +6,10 @@ from tornado.options import options
 
 from mining.urls import INCLUDE_URLS as MINING_URLS
 from admin.urls import INCLUDE_URLS as ADMIN_URLS
+from api.urls import INCLUDE_URLS as API_URLS
 
 
 CORE = [(r'/assets/(.*)', tornado.web.StaticFileHandler,
          {'path': "{}/{}".format(options.PROJECT_PATH, "assets")})]
 
-URLS = CORE + ADMIN_URLS + MINING_URLS
+URLS = CORE + ADMIN_URLS + MINING_URLS + API_URLS
