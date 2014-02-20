f01e86336355338f51e9c9ac7a3f2a85a98a7b7f
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-02-20 17:56:54 -0300

    Fix admin ng app

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -203,4 +203,32 @@ angular.module('OpenMining', ["highcharts-ng"])
     $http.get("/api/dashboard.json").success(function(data){
       $scope.dashboard_list = data;
     })
+  })
+
+  .controller('Ctrl',
+    function($scope, $http) {
+      $scope.loading = true;
+  })
+
+  .controller('ElementCube', function($scope, $http) {
+
+    angular.element(document.querySelector('#type')).bind('change', function(){
+      if (angular.element(document.querySelector('#type')).val() != "grid") {
+        $scope.chart = true;
+      } else {
+        $scope.chart = false;
+      }
+      $scope.$apply();
+    });
+
+    angular.element(document.querySelector('#cube')).bind('change', function(){
+      $scope.loading = true;
+      $http({method: 'GET',
+        url: "/admin/api/element/cube/" + angular.element(document.querySelector('#cube')).val()}).
+        success(function(data, status, headers, config) {
+          $scope._fields = data.columns;
+          $scope.loading = false;
+        });
+      $scope.$apply();
+    });
   });
