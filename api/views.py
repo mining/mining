642480da1a11e985b6bef332d44adf0231ca6d58
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-03-05 18:22:03 -0300

    implement rest api on admin crud

--- api/views.py
+++ api/views.py
@@ -7,21 +7,84 @@ import riak
 from pandas import DataFrame
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
+from redis import Redis
+from rq import Queue
 
 from settings import (RIAK_PROTOCOL, RIAK_HTTP_PORT,
                       RIAK_HOST, ADMIN_BUCKET_NAME,
                       MINING_BUCKET_NAME, MEMCACHE_CONNECTION, MEMCACHE_DEBUG)
 from admin.models import MyAdminBucket
+from utils import slugfy
 
 
-class APIDashboardHandler(tornado.web.RequestHandler):
-    def get(self):
-        get_bucket = MyAdminBucket.get('dashboard').data
-        if get_bucket is None:
-            get_bucket = []
+class ApiHandler(tornado.web.RequestHandler):
+    bucket = []
 
-        self.write(json.dumps(get_bucket))
+    @tornado.web.asynchronous
+    def get(self, slug=None):
+        bucket = self.bucket.data or []
+
+        if slug:
+            value = {}
+            for i in bucket:
+                if i.get('slug') == slug:
+                    value = i
+            bucket = value
+
+        self.write(json.dumps(bucket))
         self.finish()
+
+    def post(self):
+        data = {k: self.get_argument(k) for k in self.request.arguments}
+        data['slug'] = slugfy(data.get('name'))
+
+        bucket = [b for b in self.bucket.data or []
+                  if b['slug'] != data['slug']]
+        bucket.append(data)
+
+        MyAdminBucket.new(self.bucket.key, data=bucket).store()
+
+        self.write("Post or Put ok!")
+        self.finish()
+
+    def put(self, slug=None):
+        self.post()
+
+    @tornado.web.asynchronous
+    def delete(self, slug):
+        bucket = self.bucket.data or []
+
+        value = None
+        for i in bucket:
+            if i.get(self.bucket.key) == slug:
+                value = i.get(self.bucket.key)
+        new_bucket = [b for b in bucket if b['slug'] != slug]
+
+        MyAdminBucket.new(self.bucket.key, data=new_bucket).store()
+
+        Queue(connection=Redis()).enqueue_call(
+            func='admin.tasks.related_delete',
+            args=(self.bucket.key, slug, value)
+        )
+
+        self.write("Delete ok!")
+        self.finish()
+
+
+class Connection(ApiHandler):
+    bucket = MyAdminBucket.get('connection')
+
+
+class Cube(ApiHandler):
+    bucket = MyAdminBucket.get('cube')
+
+
+class Dashboard(ApiHandler):
+    bucket = MyAdminBucket.get('dashboard')
+
+
+class Element(ApiHandler):
+    bucket = MyAdminBucket.get('element')
 
 
 class CubeQuery(tornado.web.RequestHandler):
