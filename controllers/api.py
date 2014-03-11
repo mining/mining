bfe774a1ae1dd897256106c9b2295640c21dcb68
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-03-11 09:15:24 -0300

    generic sample used mongodb plugin (bottle)

--- controllers/api.py
+++ controllers/api.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from bottle import Bottle, response, request
+from bottle.ext.mongo import MongoPlugin
 
-from riak import RiakClient
+from bson.json_util import dumps
 
+
+ADMIN_BUCKET_NAME = 'openminig-admin'
 
 api_app = Bottle()
-ADMIN_BUCKET_NAME = 'openminig-admin'
+mongo = MongoPlugin(uri="mongodb://127.0.0.1", db=ADMIN_BUCKET_NAME,
+                    json_mongo=True)
+api_app.install(mongo)
 
 
 def api_base():
@@ -14,21 +19,8 @@ def api_base():
     response.content_type = 'application/json'
 
 
-def api_get(str_bucket, slug=None):
-    rclient = RiakClient(protocol='http', http_port=8098,
-                         host='127.0.0.1')
-    MyAdminBucket = rclient.bucket(ADMIN_BUCKET_NAME)
-
-    bucket = MyAdminBucket.get(str_bucket).data or []
-
-    if slug:
-        value = {}
-        for i in bucket:
-            if i.get('slug') == slug:
-                value = i
-        bucket = value
-
-    return bucket
+def api_get(mongodb, collection, slug=None):
+    return dumps(mongodb[collection].find())
 
 
 @api_app.route('/')
@@ -36,7 +28,6 @@ def index():
     return 'OpenMining API!'
 
 
-@api_app.route('/connection/:slug')
 @api_app.route('/connection/')
-def connection_get(slug=None):
-    return api_get('connection', slug)
+def connection_get(mongodb):
+    return api_get(mongodb, 'connection')
