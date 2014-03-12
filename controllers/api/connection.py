e5bd79a2cb80c18662a85b0fcb63b7dd8812a9ea
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-03-12 14:06:46 -0300

    set var collection name on connection api

--- controllers/api/connection.py
+++ controllers/api/connection.py
@@ -7,6 +7,7 @@ from .base import get, post, put, delete
 
 
 ADMIN_BUCKET_NAME = 'openminig-admin'
+collection = 'connection'
 
 connection_app = Bottle()
 mongo = MongoPlugin(uri="mongodb://127.0.0.1", db=ADMIN_BUCKET_NAME,
@@ -17,19 +18,19 @@ connection_app.install(mongo)
 @connection_app.route('/', method='GET')
 @connection_app.route('/:slug', method='GET')
 def connection_get(mongodb, slug=None):
-    return get(mongodb, 'connection', slug)
+    return get(mongodb, collection, slug)
 
 
 @connection_app.route('/', method='POST')
 def connection_post(mongodb, slug=None):
-    return post(mongodb, 'connection')
+    return post(mongodb, collection)
 
 
 @connection_app.route('/:slug', method='PUT')
 def connection_put(mongodb, slug=None):
-    return put(mongodb, 'connection', slug)
+    return put(mongodb, collection, slug)
 
 
 @connection_app.route('/:slug', method='DELETE')
 def connection_delete(mongodb, slug=None):
-    return delete(mongodb, 'connection', slug)
+    return delete(mongodb, collection, slug)
