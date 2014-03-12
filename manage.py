a2d1bbaa5b94ccdf3601d1a08b6d10afae2f39d2
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-03-12 16:59:54 -0300

    manage.py

--- manage.py
+++ manage.py
@@ -7,9 +7,13 @@ import argparse
 
 from bottle import static_file, Bottle, template, TEMPLATE_PATH, run
 
+from gevent import monkey
 from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+from bottle.ext.websocket import GeventWebSocketServer
 
 from controllers.api import api_app
+from controllers.stream import stream_app
 
 
 reload(sys)
@@ -29,6 +33,7 @@ TEMPLATE_PATH.insert(0, u'{}/{}'.format(PROJECT_PATH, 'views'))
 
 app = Bottle()
 app.mount('/api', api_app)
+app.mount('/stream', stream_app)
 
 
 @app.route('/asserts/<path:path>')
@@ -45,16 +50,14 @@ def main():
     print u'OpenMining start server at: {}:{}'.format(args.ip,
                                                       args.port)
 
+    monkey.patch_all()
     if args.debug is None:
-        from gevent import monkey
-
-        monkey.patch_all()
-
-        server = WSGIServer((args.ip, args.port), app)
+        server = WSGIServer((args.ip, args.port), app,
+                            handler_class=WebSocketHandler)
         server.serve_forever()
 
     run(app=app, host=args.ip, port=args.port, debug=args.debug,
-        reloader=True)
+        reloader=True, server=GeventWebSocketServer)
 
 
 if __name__ == "__main__":
