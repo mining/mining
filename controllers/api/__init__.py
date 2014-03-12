a4ed10fd48700c8cfac87e9f36f53e9b373c7c40
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-03-12 14:15:33 -0300

    Create dashboard api

--- controllers/api/__init__.py
+++ controllers/api/__init__.py
@@ -5,6 +5,7 @@ from bottle import Bottle
 from .connection import connection_app
 from .cube import cube_app
 from .element import element_app
+from .dashboard import dashboard_app
 
 
 ADMIN_BUCKET_NAME = 'openminig-admin'
@@ -13,6 +14,7 @@ api_app = Bottle()
 api_app.mount('/connection', connection_app)
 api_app.mount('/cube', cube_app)
 api_app.mount('/element', element_app)
+api_app.mount('/dashboard', dashboard_app)
 
 
 @api_app.route('/')
---
+++ controllers/api/dashboard.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from bottle import Bottle
+from bottle.ext.mongo import MongoPlugin
+
+from .base import get, post, put, delete
+
+
+ADMIN_BUCKET_NAME = 'openminig-admin'
+collection = 'dashboard'
+
+dashboard_app = Bottle()
+mongo = MongoPlugin(uri="mongodb://127.0.0.1", db=ADMIN_BUCKET_NAME,
+                    json_mongo=True)
+dashboard_app.install(mongo)
+
+
+@dashboard_app.route('/', method='GET')
+@dashboard_app.route('/:slug', method='GET')
+def dashboard_get(mongodb, slug=None):
+    return get(mongodb, collection, slug)
+
+
+@dashboard_app.route('/', method='POST')
+def dashboard_post(mongodb, slug=None):
+    return post(mongodb, collection)
+
+
+@dashboard_app.route('/:slug', method='PUT')
+def dashboard_put(mongodb, slug=None):
+    return put(mongodb, collection, slug)
+
+
+@dashboard_app.route('/:slug', method='DELETE')
+def dashboard_delete(mongodb, slug=None):
+    return delete(mongodb, collection, slug)
