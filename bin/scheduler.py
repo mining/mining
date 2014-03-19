7ac2f181fcba6dfb3be2172c32126d9736f6fe4a
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-03-19 11:59:24 -0300

    implemented schedule, ref #75

--- bin/scheduler.py
+++ bin/scheduler.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from gevent import monkey
+monkey.patch_all()
+
 import time
 import schedule
 
@@ -12,19 +15,33 @@ from bin.mining import run
 def job(slug):
     run(slug)
 
-"""
-schedule.every(1).minutes.do(cube)
-schedule.every().hour.do(cube)
-schedule.every().day.at("16:04").do(cube)
-"""
+
+def rules(cube):
+    scheduler_type = cube.get('scheduler_type', 'minutes')
+    scheduler_interval = cube.get('scheduler_interval', 60)
+
+    if scheduler_type == 'minutes':
+        t = schedule.every(scheduler_interval).minutes
+    elif scheduler_type == 'hour':
+        t = schedule.every().hour
+    elif scheduler_type == 'day':
+        t = schedule.every().day
+    t.do(job, slug=cube.get('slug'))
+
 
 mongo = MongoPlugin(uri=MONGO_URI, db=ADMIN_BUCKET_NAME,
                     json_mongo=True).get_mongo()
 
-for s in mongo['scheduler'].find():
-    t = getattr(schedule.every(s.get('interval')), s.get('type'))
-    t.do(job, slug=s.get('slug'))
+register = []
+for cube in mongo['cube'].find({'scheduler': True}):
+    rules(cube)
+    register.append(cube['slug'])
+
 
 while True:
+    for cube in mongo['cube'].find({'scheduler': True}):
+        if cube['slug'] not in register:
+            rules(cube)
+
     schedule.run_pending()
     time.sleep(1)
