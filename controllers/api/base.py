cf5e0ac924aae27ef890cc2aeeda206f76b9cf93
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-03-27 15:56:53 -0300

    add new parans field on base api used to set new key on database

--- controllers/api/base.py
+++ controllers/api/base.py
@@ -25,12 +25,12 @@ def get(mongodb, collection, slug):
     return dumps(response)
 
 
-def post(mongodb, collection, opt={}):
+def post(mongodb, collection, opt={}, field={'key': 'slug', 'value': 'name'}):
     base()
     data = request.json
-    data['slug'] = slugfy(data['name'])
+    data[field['key']] = slugfy(data[field['value']])
     data = dict(data.items() + opt.items())
-    get = mongodb[collection].find({'slug': data['slug']})
+    get = mongodb[collection].find({field['key']: data[field['key']]})
     if get.count() == 0:
         mongodb[collection].insert(data)
         del data['_id']
@@ -38,20 +38,21 @@ def post(mongodb, collection, opt={}):
     return {'status': 'error', 'message': 'Object exist, please send PUT!'}
 
 
-def put(mongodb, collection, slug, opt={}):
+def put(mongodb, collection, slug, opt={}, field={'key': 'slug'},
+        request_json=request.json):
     base()
-    data = request.json
-    data['slug'] = slug
+    data = request_json
+    data[field['key']] = slug
     data = dict(data.items() + opt.items())
-    get = mongodb[collection].find_one({'slug': slug})
+    get = mongodb[collection].find_one({field['key']: slug})
     if get:
-        mongodb[collection].update({'slug': slug}, data)
+        mongodb[collection].update({field['key']: slug}, data)
         return data
     return {'status': 'error',
             'message': 'Object not exist, please send POST to create!'}
 
 
-def delete(mongodb, collection, slug):
+def delete(mongodb, collection, slug, field={"key": "slug"}):
     base()
     mongodb[collection].remove({'slug': slug})
     return {'status': 'success'}
