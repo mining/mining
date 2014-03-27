2ca8f91a61e0b2963b865f8a21d5b588dbbd64b8
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-03-27 15:57:52 -0300

    make user api

--- controllers/api/__init__.py
+++ controllers/api/__init__.py
@@ -6,15 +6,15 @@ from .connection import connection_app
 from .cube import cube_app
 from .element import element_app
 from .dashboard import dashboard_app
+from .user import user_app
 
-
-ADMIN_BUCKET_NAME = 'openminig-admin'
 
 api_app = Bottle()
 api_app.mount('/connection', connection_app)
 api_app.mount('/cube', cube_app)
 api_app.mount('/element', element_app)
 api_app.mount('/dashboard', dashboard_app)
+api_app.mount('/user', user_app)
 
 
 @api_app.route('/')
---
+++ controllers/api/user.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import uuid
+import hmac
+import json
+
+try:
+    from hashlib import sha1
+except ImportError:
+    import sha
+    sha1 = sha.sha
+
+from bottle import Bottle, request
+from bottle.ext.mongo import MongoPlugin
+from bson.json_util import dumps
+
+from utils import conf
+from .base import get, post, put, delete
+
+collection = 'user'
+
+user_app = Bottle()
+mongo = MongoPlugin(
+    uri=conf("mongodb")["uri"],
+    db=conf("mongodb")["db"],
+    json_mongo=True)
+user_app.install(mongo)
+
+
+@user_app.route('/', method='GET')
+@user_app.route('/<slug>', method='GET')
+def user_get(mongodb, slug=None):
+    _get = json.loads(get(mongodb, collection, slug))
+    data = []
+    for _g in _get:
+        _g.pop('password', None)
+        _g.pop('apikey', None)
+        data.append(_g)
+    return json.dumps(data)
+
+
+@user_app.route('/', method='POST')
+def user_post(mongodb, slug=None):
+    new_uuid = uuid.uuid4()
+    opt = {}
+    opt['apikey'] = hmac.new(new_uuid.bytes, digestmod=sha1).hexdigest()
+    return post(mongodb, collection, opt,
+                {'key': 'username', 'value': 'username'})
+
+
+@user_app.route('/<slug>', method='PUT')
+def user_put(mongodb, slug=None):
+    data = request.json
+    data.pop("apikey", None)
+    return put(mongodb, collection, slug, field={'key': 'username'},
+               request_json=data)
+
+
+@user_app.route('/<slug>', method='DELETE')
+def user_delete(mongodb, slug=None):
+    return delete(mongodb, collection, slug)
