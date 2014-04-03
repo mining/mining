a7d3ebf40567224b3dad9ce187605a62fe8c7af5
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-04-03 17:36:06 -0300

    writing auth import module

---
+++ auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from bottle.ext import auth
+
+from utils import conf
+
+
+try:
+    auth_import = conf('auth')['engine'].split('.')[-1]
+    auth_from = u".".join(conf('auth')['engine'].split('.')[:-1])
+    auth_engine = getattr(__import__(auth_from, fromlist=[auth_import]),
+                          auth_import)
+except:
+    print 'Set valid auth engine'
+    exit(0)
+
+callback = u"{}://{}:{}".format(
+    conf('openmining')['protocol'],
+    conf('openmining')['domain'],
+    conf('openmining')['port'])
+
+if auth_import == 'Google':
+    engine = auth_engine(
+        conf('auth')['key'], conf('auth')['secret'], callback)
+elif auth_import == 'Facebook':
+    #  Not working requered parans
+    engine = auth_engine()
+elif auth_import == 'Twitter':
+    #  Not working requered parans
+    engine = auth_engine()
+else:
+    engine = auth_engine()
+
+auth = auth.AuthPlugin(engine)
