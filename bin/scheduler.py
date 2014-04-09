9a511804aaf8899a497816491dbdca57f4965700
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-04-09 12:05:08 -0300

    try process, if Exception generate log

--- bin/scheduler.py
+++ bin/scheduler.py
@@ -14,6 +14,9 @@ from bin.mining import run
 from utils import conf, log_it
 
 
+log_it("START", "bin-scheduler")
+
+
 def job(slug):
     log_it("RUN JOB: {}".format(slug), "bin-scheduler")
     run(slug)
@@ -21,23 +24,26 @@ def job(slug):
 
 def rules(cube):
     scheduler_type = cube.get('scheduler_type', 'minutes')
-    scheduler_interval = cube.get('scheduler_interval', 60)
+    scheduler_interval = int(cube.get('scheduler_interval', 60))
 
     log_it("START REGISTER", "bin-scheduler")
     log_it("cube: {}".format(cube.get('slug')), "bin-scheduler")
     log_it("type: {}".format(scheduler_type), "bin-scheduler")
-    log_it("interval: {}".format(scheduler_type), "bin-scheduler")
+    log_it("interval: {}".format(scheduler_interval), "bin-scheduler")
     log_it("END REGISTER", "bin-scheduler")
 
+    t = {}
     if scheduler_type == 'minutes':
-        t = schedule.every(int(scheduler_interval)).minutes
+        t = schedule.every(scheduler_interval).minutes
     elif scheduler_type == 'hour':
         t = schedule.every().hour
     elif scheduler_type == 'day':
         t = schedule.every().day
-    else:
-        return None
-    t.do(job, slug=cube.get('slug'))
+
+    try:
+        t.do(job, slug=cube.get('slug'))
+    except Exception, e:
+        log_it("ERROR {}: {}".format(cube.get('slug'), e))
 
 
 mongo = MongoPlugin(
