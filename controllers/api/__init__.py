c8199eeebf460ed04763e3f88ff8456f895d9a71
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-05-13 12:08:23 -0300

    writting user group API, ref #98

--- controllers/api/__init__.py
+++ controllers/api/__init__.py
@@ -7,6 +7,7 @@ from .cube import cube_app
 from .element import element_app
 from .dashboard import dashboard_app
 from .user import user_app
+from .group import group_app
 from .filter import filter_app
 
 
@@ -16,6 +17,7 @@ api_app.mount('/cube', cube_app)
 api_app.mount('/element', element_app)
 api_app.mount('/dashboard', dashboard_app)
 api_app.mount('/user', user_app)
+api_app.mount('/group', group_app)
 api_app.mount('/filter', filter_app)
 
 
