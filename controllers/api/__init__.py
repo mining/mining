92761ca1daee49948ff9f1f32b9ef3a071d4778d
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-05-13 14:48:07 -0300

    writting widget api, ref #128

--- controllers/api/__init__.py
+++ controllers/api/__init__.py
@@ -5,6 +5,7 @@ from bottle import Bottle
 from .connection import connection_app
 from .cube import cube_app
 from .element import element_app
+from .widget import widget_app
 from .dashboard import dashboard_app
 from .user import user_app
 from .group import group_app
@@ -15,6 +16,7 @@ api_app = Bottle()
 api_app.mount('/connection', connection_app)
 api_app.mount('/cube', cube_app)
 api_app.mount('/element', element_app)
+api_app.mount('/widget', widget_app)
 api_app.mount('/dashboard', dashboard_app)
 api_app.mount('/user', user_app)
 api_app.mount('/group', group_app)
