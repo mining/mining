3677136e6026b8d64d53638e288044c506d7794f
Yuri Piratello <yuri.piratello@gmail.com>
2014-05-30 14:46:51 -0300

    more translates

--- mining/assets/app/scripts/i18n/pt_BR.js
+++ mining/assets/app/scripts/i18n/pt_BR.js
@@ -1,5 +1,5 @@
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
-    gettextCatalog.setStrings('pt_BR', {"Connection":"Conexão","Cube":"Cubo","Dashboard Group":"Grupo de Dashboard","Data":"Dados","Element":"Elementos","Late Scheduler":"Agendamentos Atrasados","Logout":"Sair","Others":"Outros","Permission Group":"Grupo de Permissões","Refresh":"Atualizar","Settings":"Configurações","Users":"Usuários","Search...":"Pesquisar..."});
+    gettextCatalog.setStrings('pt_BR', {"Connection":"Conexão","Cube":"Cubo","Dashboard Group":"Grupo de Dashboard","Data":"Dados","Element":"Elementos","Late Scheduler":"Agendamentos Atrasados","Logout":"Sair","Others":"Outros","Permission Group":"Grupo de Permissões","Refresh":"Atualizar","Refresh list":"Atualizar lista","Settings":"Configurações","Users":"Usuários","Search...":"Pesquisar..."});
 /* jshint +W100 */
 }]);
