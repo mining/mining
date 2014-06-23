3480d464c7d1ae9de7f69bb5bb0c0411c7c7d2af
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-06-23 09:52:16 -0300

    writting #LevelDB adapter

---
+++ mining/db/backends/mleveldb.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+from leveldb import LevelDB as DB
+
+
+class LevelDB(object):
+    def conn(self):
+        """Open connection on LevelDB DataBase"""
+        conn = DB("/tmp/{}.mining".format(self.conf.get("db")))
+        return conn
+
+    def save(self, house, data, content_type="application/json"):
+        """Save meta dada on LevelDB"""
+        if content_type == "application/json":
+            return self.conn().Put(house, json.dumps(data))
+        return self.conn().Put(house, data)
+
+    def get(self, house, content_type="application/json", callback={}):
+        """Get bucket"""
+        if content_type == "application/json":
+            return json.loads(self.conn().Get(house)) or callback
+        return self.conn().Get(house) or callback
