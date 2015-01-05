d555dc016fbfa2828ed03610c3457e14ba5486cd
Thiago Avelino <thiagoavelinoster@gmail.com>
2015-01-05 16:42:43 -0200

    removed frontend files move frontend files to a new repository

--- mining/assets/app/scripts/admin/resolvers.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-admin['resolvers'] = {
-  'connection': {
-    'getConnections': ['Connection',function(Connection){
-      return Connection.get();
-    }]
-  }
-};
--- mining/assets/app/scripts/report/services.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-report
-  .factory('LineChart', function($http){
-    return {
-      'getConfig':function(URL){
-        return $http.post(URL)
-      }
-    };
-  })
