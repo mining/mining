0204cec052b6d4b78ac85fa87149dcf32d1ddc14
Thiago Avelino <thiagoavelinoster@gmail.com>
2015-01-20 00:48:34 -0200

    update redis and leveldb, used GenericDataWarehouse

--- mining/db/backends/mleveldb.py
+++ mining/db/backends/mleveldb.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 import json
 from leveldb import LevelDB as DB
+from mining.db.datawarehouse import GenericDataWarehouse
 
 
-class LevelDB(object):
+class LevelDB(GenericDataWarehouse):
     def conn(self):
         """Open connection on LevelDB DataBase"""
         conn = DB("/tmp/{}.mining".format(self.conf.get("db")))
@@ -14,10 +15,11 @@ class LevelDB(object):
         """Save meta dada on LevelDB"""
         if content_type == "application/json":
             return self.conn().Put(house, json.dumps(data))
-        return self.conn().Put(house, data)
+        return self.conn.Put(house, data)
 
-    def get(self, house, content_type="application/json", callback={}):
+    def get(self, house, content_type="application/json", callback={},
+            filters=[], page=1):
         """Get bucket"""
         if content_type == "application/json":
             return json.loads(self.conn().Get(house)) or callback
-        return self.conn().Get(house) or callback
+        return self.conn.Get(house) or callback
--- mining/db/backends/mredis.py
+++ mining/db/backends/mredis.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 import json
 from redis import StrictRedis
+from mining.db.datawarehouse import GenericDataWarehouse
 
 
-class Redis(object):
+class Redis(GenericDataWarehouse):
     def conn(self):
         """Open connection on Redis DataBase"""
         conn = StrictRedis(host=self.conf.get('host'),
@@ -16,10 +17,11 @@ class Redis(object):
         """Save meta dada on Redis"""
         if content_type == "application/json":
             return self.conn().set(house, json.dumps(data))
-        return self.conn().set(house, data)
+        return self.conn.set(house, data)
 
-    def get(self, house, content_type="application/json", callback={}):
+    def get(self, house, content_type="application/json", callback={},
+            filters=[], page=1):
         """Get bucket"""
         if content_type == "application/json":
             return json.loads(self.conn().get(house)) or callback
-        return self.conn().get(house) or callback
+        return self.conn.get(house) or callback
