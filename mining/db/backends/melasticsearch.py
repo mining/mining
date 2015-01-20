c545d17c7fb8ca9483c9d35ca57eee7fd57eccac
Thiago Avelino <thiagoavelinoster@gmail.com>
2015-01-20 00:56:00 -0200

    upgrade elasticsearch GenericDataWarehouse

--- mining/db/backends/melasticsearch.py
+++ mining/db/backends/melasticsearch.py
@@ -5,29 +5,28 @@ from bottle import request
 from elasticsearch import RequestsHttpConnection, Elasticsearch as ES
 
 from mining.utils.listc import listc_dict
+from mining.db.datawarehouse import GenericDataWarehouse
 
 
-class Elasticsearch(object):
+class Elasticsearch(GenericDataWarehouse):
 
-    _search = True
-
-    def conn(self):
+    def __init__(self):
         """Open connection on Elasticsearch DataBase"""
-        conn = ES([{"host": self.conf.get('host'),
-                    "port": int(self.conf.get('port'))}],
-                  connection_class=RequestsHttpConnection)
-        return conn
+        super(Elasticsearch, self).__init__()
+        self.search = True
+        self.conn = ES([{"host": self.conf.get('host'),
+                         "port": int(self.conf.get('port'))}],
+                       connection_class=RequestsHttpConnection)
+        self.base_url = self.conn.transport.get_connection().base_url
 
     def _bulk(self, bulk):
-        requests.post("http://{}:{}/_bulk".format(
-            self.conf.get('host'), self.conf.get('port')),
+        requests.post("{}/_bulk".format(self.base_url),
             headers={'content-type': 'application/json'},
             data=bulk)
 
     def save(self, house, data, content_type='dict'):
         """Save meta dada on Elasticsearch"""
-        requests.delete("http://{}:{}/{}".format(
-            self.conf.get('host'), self.conf.get('port'), house))
+        requests.delete("{}/{}".format(self.base_url, house))
         bulk = ""
         for i, obj in enumerate(data.get('data')):
             bulk += "{}\n".format(json.dumps({"index":
@@ -39,27 +38,27 @@ class Elasticsearch(object):
                 self._bulk(bulk)
                 bulk = ""
         self._bulk(bulk)
-        self.conn().index(index=house,
-                          doc_type='columns',
-                          body={"columns": data.get('columns')})
-        return self.conn()
+        self.conn.index(index=house,
+                        doc_type='columns',
+                        body={"columns": data.get('columns')})
+        return self.conn
 
-    def get(self, house, filters=None, page=1, content_type="dict",
+    def get(self, house, filters=[], page=1, content_type="dict",
             callback={}):
         """Get meta data on Elasticsearch"""
         doc_args = {"size": 50, "from_": page - 1}
         if not page:
             doc_args = {}
-        count = self.conn().count(
+        count = self.conn.count(
             index=house, doc_type="data",
             body=self.filter(filters=filters)).get('count')
-        doc_data = self.conn().search(index=house, doc_type='data',
-                                      body=self.filter(filters=filters),
-                                      **doc_args)
+        doc_data = self.conn.search(index=house, doc_type='data',
+                                    body=self.filter(filters=filters),
+                                    **doc_args)
         data = {}
         data['data'] = listc_dict(doc_data.get('hits').get('hits'), "_source")
-        doc_columns = self.conn().search(index=house, doc_type='columns',
-                                         body=self.filter())
+        doc_columns = self.conn.search(index=house, doc_type='columns',
+                                       body=self.filter())
         data.update(doc_columns.get('hits').get('hits')[0].get('_source'))
         data['count'] = count
         return data
